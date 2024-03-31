64a33e0daf71c789a3fbff6380148bfbcf9f8ce7
larry99yyz <143693593+larry99yyz@users.noreply.github.com>
2024-03-31 11:40:40 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-# app/routes.py
 from flask import Flask, request, jsonify
 from app.models import EmailScanRequest
 from app.utils import scan_email
@@ -11,6 +10,16 @@ def scan_email_endpoint():
     try:
         email_request = EmailScanRequest(**data)
         result = scan_email(email_request.email_id)
+        
+        if result == "SPAM":
+            notification_message = f"Email '{email_request.email_id}' detected as spam. Moved to trash."
+            # Code for notifying owner_email
+            # send_notification(email_request.owner_email, notification_message)
+        else:
+            notification_message = f"Email '{email_request.email_id}' is not spam."
+            # Code for notifying owner_email
+            # send_notification(email_request.owner_email, notification_message)
+
         return jsonify({"message": "Email scan initiated.", "result": result}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
